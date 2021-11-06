30dcd4481d0651f8092832411fb5aff942494d6f
Alexander Lozhkin <syberside@gmail.com>
2021-11-06 12:08:15 +0300

    ASP.NET Web App boilerplate

---
+++ src/aTES/aTES.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aTES.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
